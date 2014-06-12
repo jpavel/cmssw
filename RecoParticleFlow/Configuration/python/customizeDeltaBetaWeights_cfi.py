1f292f3b2a2168aeca55e9a12850b3be7bdb5453
Pavel Jez <pavel.jez@cern.ch>
2014-06-12 17:39:44 +0200

    added customization function suggested by L. Gray

---
+++ RecoParticleFlow/Configuration/python/customizeDeltaBetaWeights_cfi.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+from CommonTools.ParticleFlow.Isolation.tools_cfi import *
+
+
+def customize(process):
+
+    if hasattr(process,'pfParticleSelectionSequence'): 
+        process.load("CommonTools.ParticleFlow.deltaBetaWeights_cff")
+        process.pfParticleSelectionSequence += process.pfDeltaBetaWeightingSequence
+
+    if hasattr(process,'elPFIsoDepositNeutral'):
+        process.elPFIsoDepositNeutral=isoDepositReplace('pfElectronTranslator:pf','pfWeightedNeutralHadrons')
+
+    if hasattr(process,'elPFIsoDepositGamma'):
+        process.elPFIsoDepositGamma=isoDepositReplace('pfElectronTranslator:pf','pfWeightedPhotons')
+
+    if hasattr(process,'gedElPFIsoDepositNeutral'):
+        process.gedElPFIsoDepositNeutral=isoDepositReplace('gedGsfElectronsTmp','pfWeightedNeutralHadrons')
+
+    if hasattr(process,'gedElPFIsoDepositGamma'):
+        process.gedElPFIsoDepositGamma=isoDepositReplace('gedGsfElectronsTmp','pfWeightedPhotons')
+
+    if hasattr(process,'muPFIsoDepositNeutral'):
+       process.muPFIsoDepositNeutral=isoDepositReplace('muons1stStep','pfWeightedNeutralHadrons')
+
+    if hasattr(process,'muPFIsoDepositGamma'):
+        process.muPFIsoDepositGamma=isoDepositReplace('muons1stStep','pfWeightedPhotons')
+
+    if hasattr(process,'phPFIsoDepositNeutral'):
+       process.phPFIsoDepositNeutral=isoDepositReplace('pfSelectedPhotons','pfWeightedNeutralHadrons')
+
+    if hasattr(process,'phPFIsoDepositGamma'):
+        process.phPFIsoDepositGamma.ExtractorPSet.inputCandView = cms.InputTag("pfWeightedPhotons")
+
+
+    return process
+
