9b0cb404eb0467e43ca81bdf53f117ace986be04
Pavel Jez <pavel.jez@cern.ch>
2014-06-24 15:26:05 +0200

    residual fixes for exercise 5

--- PhysicsTools/PatExamples/python/customizedSelection_cff.py
+++ PhysicsTools/PatExamples/python/customizedSelection_cff.py
@@ -3,12 +3,12 @@ import FWCore.ParameterSet.Config as cms
 
 isolatedPatElectrons = cms.EDFilter("PATElectronSelector",
                                         src = cms.InputTag("selectedPatElectrons"),
-                                        cut = cms.string("pt>10 & abs(eta)<2.5 & (trackIso+caloIso)/pt<0.05")
+                                        cut = cms.string("pt>10 & abs(eta)<2.5 & (trackIso+caloIso)/pt< 5")
                                     )
 
 isolatedPatMuons = cms.EDFilter("PATMuonSelector",
                                 src = cms.InputTag("selectedPatMuons"),
-                                cut = cms.string("pt>10 & abs(eta)<2.5 & (trackIso+caloIso)/pt<0.05")
+                                cut = cms.string("pt>10 & abs(eta)<2.5 & (trackIso+caloIso)/pt< 5")
                                 )
 
 customSelection = cms.Sequence(
